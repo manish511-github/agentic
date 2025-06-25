627399092bd3257224ac53e57222e10768c54e98
maneesh@511 <singh.maneesh50@gmail.com>
2025-06-25 20:04:39 +0530

    fix (agents) : import error issues

--- ragent/app/core/agents_tasks/rdagent/rd_agent_v1/graph.py
+++ ragent/app/core/agents_tasks/rdagent/rd_agent_v1/graph.py
@@ -23,7 +23,7 @@ def create_reddit_graph() -> StateGraph:
     graph.add_edge("fetch_posts_node", END)
     return graph.compile()
 
-# Basic Reddot Agent Call -
+# Basic Reddit Agent Call -
 def create_basic_reddit_graph() -> StateGraph:
     graph = StateGraph(AgentState)
     graph.add_node("validate_input", validate_input_node)
@@ -31,12 +31,12 @@ def create_basic_reddit_graph() -> StateGraph:
     graph.add_node("search_posts_directly_node", search_posts_directly_node)
     graph.add_node("search_subreddits_node", search_subreddits_node)
     graph.add_node("fetch_basic_post_node", fetch_basic_post_nodes)
-    graph.set_entry_point("fetch_basic_post_node")
+    graph.set_entry_point("validate_input")
 
     graph.add_edge("validate_input", "generate_queries_node")
     graph.add_edge("generate_queries_node", "search_posts_directly_node")
     graph.add_edge("search_posts_directly_node", "search_subreddits_node")
-    graph.add_edge("search_subreddits_node", "fetch_basic_post_nodes")
+    graph.add_edge("search_subreddits_node", "fetch_basic_post_node")
     graph.add_edge("fetch_basic_post_node", END)
     return graph.compile()
 
